266a2bbf7e824a4426915973ad8ba24b69a9da32
Anush Pote <anushpote345@gmail.com>
2026-05-19 21:07:13 +0545

    Counter

--- day4.py
+++ day4.py
@@ -4,10 +4,9 @@
 # #Looping Statements
 # 1. Finite Loop (for _ in )
 
+# for i in range(1, 5, 2):    # 1 is starting value, 5 is stopping value, 5 is excluded, 2 is called step value
+#     print("Welcome home", i)
 
-
-for i in range(1, 5, 2):    # 1 is starting value, 5 is stopping value, 5 is excluded, 2 is called step value
-    print("Welcome home", i)
 
 # Multiplication Table
 # Table of 2
@@ -22,7 +21,20 @@ for i in range(1, 5, 2):    # 1 is starting value, 5 is stopping value, 5 is exc
 # for num in range(1, 11):
 #     print(f"{number} * {num} = {number*num}")
 
+# for and for each loop
 sentence = "We love Nepal"
+vowels = "aeiouAEIOU" 
+count = 0
 
-for char in sentence:
-    print(char)
+for each_char in sentence:
+    #check if its a vowel
+    if each_char in vowels:
+        count += 1
+        print(each_char)
+        
+    elif each_char==" ":
+        print("Here is space")
+    else:
+        print(f"{each_char} is a consonant")
+print(f"There are {count} vowels")
+    
