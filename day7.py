7c7fa692cf1027238f3b6d0d8de7a1ade7448851
Anush Pote <anushpote345@gmail.com>
2026-05-22 20:34:35 +0545

    Default Parameter

--- day7.py
+++ day7.py
@@ -2,10 +2,19 @@
 # Function is a block code that represents a specific task
 # Function is a variable that store group of code
 
-def welcome_message():  #Function Definition
-    print("Welcome Ram")
+# def welcome_message():  #Function Definition
+#     print("Welcome Ram")
+#     print("Please check your email and verify your account")
+
+# welcome_message # Function call
+
+# print(welcome_message())
+
+# If a paremeter has a default value, its called a defaul paramenter
+def welcome_message(name, greeting="Welcome"):  # Only return "Welcome" in greeting if greeting is not given as argument/ empty
+    print(greeting, name)
     print("Please check your email and verify your account")
 
-welcome_message # Function call
-
-print(welcome_message())
+# Actual data sent for a parameter, for e.g. "Ram is called as argument. But in general it can simply be called as para/meter"
+welcome_message("Ram", "Good Morning") # Returns -> Good Morning Ram
+welcome_message("Ram") # Return -> Welcome Ram
