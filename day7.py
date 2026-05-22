938fe2d7bc725bbbfa0f5cacba90734ab2605af0
Anush Pote <anushpote345@gmail.com>
2026-05-22 20:38:31 +0545

    Keyword Argument

--- day7.py
+++ day7.py
@@ -16,5 +16,6 @@ def welcome_message(name, greeting="Welcome"):  # Only return "Welcome" in greet
     print("Please check your email and verify your account")
 
 # Actual data sent for a parameter, for e.g. "Ram is called as argument. But in general it can simply be called as para/meter"
-welcome_message("Ram", "Good Morning") # Returns -> Good Morning Ram
-welcome_message("Ram") # Return -> Welcome Ram
+# welcome_message("Ram", "Good Morning") # Returns -> Good Morning Ram
+# welcome_message("Ram") # Return -> Welcome Ram
+welcome_message(name="Ram", greeting="Hello")   # This usage is called as keyword argument
