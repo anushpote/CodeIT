4a1221e299de07ca5861c74e04ed96748c4cd445
Anush Pote <anushpote345@gmail.com>
2026-05-22 20:22:10 +0545

    Function

---
+++ day7.py
@@ -0,0 +1,11 @@
+# Function
+# Function is a block code that represents a specific task
+# Function is a variable that store group of code
+
+def welcome_message():  #Function Definition
+    print("Welcome Ram")
+    print("Please check your email and verify your account")
+
+welcome_message # Function call
+
+print(welcome_message())
