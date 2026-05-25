76cb3d944709c51749d356bd057a91d1ab04fe27
Anush Pote <anushpote345@gmail.com>
2026-05-25 20:08:59 +0545

    OTP Generation

---
+++ Daily/day9.py
@@ -0,0 +1,10 @@
+# basic otp generation
+# import random
+
+# #Generate 6-digit OTP
+# otp = random.randint(100000, 999999)
+
+# print("Your OTP is: ", otp)
+
+###########################################
+
