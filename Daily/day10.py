146d65d2850a58c1a593a31108fc130bcb1eb4dc
Anush Pote <anushpote345@gmail.com>
2026-05-26 20:19:12 +0545

    Reading line by line

--- Daily/day10.py
+++ Daily/day10.py
@@ -1,5 +1,13 @@
-# 1. Reading a Text File
-with open("mytext.txt", "r") as file:
-    content = file.read()
+# # 1. Reading a Text File
+# with open("mytext.txt", "r") as file:
+#     content = file.read()
 
-print(content)
+# print(content)
+
+# 1. Reading a Text File line by line
+# with open("mytext.txt", "r") as file:
+#     #content = file.read()
+#     for line in file.readlines():
+#         print(line)
+
+
