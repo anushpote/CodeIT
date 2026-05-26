a357dbf1c5001c6c13ff821f22a052b695f3f4f9
Anush Pote <anushpote345@gmail.com>
2026-05-26 20:36:53 +0545

    WPA/WPS Tester

--- Daily/day10.py
+++ Daily/day10.py
@@ -4,10 +4,23 @@
 
 # print(content)
 
+###############################################
+
 # 1. Reading a Text File line by line
 # with open("mytext.txt", "r") as file:
 #     #content = file.read()
 #     for line in file.readlines():
 #         print(line)
 
+###############################################
 
+# WPA/WPS Tester
+
+wifi_password = "admin123"  # Pretend the password is encoded
+with open("mytext.txt","r") as passwords_file:
+    for password in passwords_file:
+        cleaned_pass = password.strip() # strip(): remove the new line that spawns when you press enter
+        #print(cleaned_pass, len(cleaned_pass)) # not necessary to print this line
+
+        if cleaned_pass == wifi_password:
+            print("Password Found", cleaned_pass)
