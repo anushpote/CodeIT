fda26963a40b471cf50ed9e88c22f4fd57ec45f5
Anush Pote <anushpote345@gmail.com>
2026-05-27 20:09:44 +0545

    try and except

---
+++ Daily/day11.py
@@ -0,0 +1,12 @@
+# Exception Handling
+
+#1a = 5  # Syntax error. Not exception
+
+try:
+    user_input = int(input("Enter a number: "))
+except:
+    print("Something went wrong!")
+
+print("Something running")
+
+print("Program ends here")
