0582317d3972d99c726f8ae285d506b665ec830b
Anush Pote <anushpote345@gmail.com>
2026-05-27 20:21:00 +0545

    ValueError Handling

--- Daily/day11.py
+++ Daily/day11.py
@@ -2,12 +2,12 @@
 
 #1a = 5  # Syntax error. Not exception
 
-user_input = int(input("Enter a number: "))
-
-# try:
-#     user_input = int(input("Enter a number: "))
-# except Exception as e:
-#     print("Something went wrong!", e)
+try:
+    user_input = int(input("Enter a number: "))
+except ValueError:
+    print("Please enter numeric value only. For example: 123")
+except Exception as e2:
+    print("Something went wrong!", e2)
 
 print("Something running")
 
