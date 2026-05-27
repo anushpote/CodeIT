b907b2aefd0cd289790bb99a302c2db5c5940706
Anush Pote <anushpote345@gmail.com>
2026-05-27 20:30:06 +0545

    try, except and finally

--- Daily/day11.py
+++ Daily/day11.py
@@ -2,16 +2,30 @@
 
 #1a = 5  # Syntax error. Not exception
 
-while True:
-    try:
-        user_input = int(input("Enter a number: "))
-    except ValueError:  # if user enter wrong value
-        print("Please enter numeric value only. For example: 123")
-    except Exception as e2:
-        print("Something went wrong!", e2)
-    else:
-        break
+#   VALUE ERROR
+# while True:
+#     try:
+#         user_input = int(input("Enter a number: "))
+#     except ValueError:  # if user enter wrong value
+#         print("Please enter numeric value only. For example: 123")
+#     except Exception as e2:
+#         print("Something went wrong!", e2)
+#     else:
+#         break
 
-print("Something running")
+# print("Something running")
 
-print("Program ends here")
+# print("Program ends here")
+
+##############################################################################
+
+students = ["ram","hari","shyam"]
+
+try:
+    students.remove("shyam")
+except ValueError:
+    print("The item you are trying to remove does not exist.")
+else:
+    print("Removed successfully")
+finally:
+    print("Student Removal program completed")
