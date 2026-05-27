f53867862c9cc2e87f62cdb11fde8bedd82beae0
Anush Pote <anushpote345@gmail.com>
2026-05-27 20:24:55 +0545

    Proper Exception Handling of ValueError

--- Daily/day11.py
+++ Daily/day11.py
@@ -2,12 +2,15 @@
 
 #1a = 5  # Syntax error. Not exception
 
-try:
-    user_input = int(input("Enter a number: "))
-except ValueError:
-    print("Please enter numeric value only. For example: 123")
-except Exception as e2:
-    print("Something went wrong!", e2)
+while True:
+    try:
+        user_input = int(input("Enter a number: "))
+    except ValueError:  # if user enter wrong value
+        print("Please enter numeric value only. For example: 123")
+    except Exception as e2:
+        print("Something went wrong!", e2)
+    else:
+        break
 
 print("Something running")
 
