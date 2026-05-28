b2f9f453e7325573193370356a146e04b494e9a8
Anush Pote <anushpote345@gmail.com>
2026-05-28 20:55:42 +0545

    Use case of lambda function

--- Daily/day12.py
+++ Daily/day12.py
@@ -24,18 +24,48 @@
 # print(next(int_marks))  # lazy evaluation
 # print(next(int_marks))  # lazy evaluation
 
-marks = ['45','26','50','76']
+marks = ['100','45','26','50','76']
 
 int_marks = list(map(int, marks))
 
-print(int_marks)
+# print(int_marks)
 
-# def pass_fail(mark):
-#     if mark >= 30:
-#         return 'P'
-#     else:
-#         return 'F'
+# # def pass_fail(mark):
+# #     if mark >= 30:
+# #         return 'P'
+# #     else:
+# #         return 'F'
 
-marks_to_pass_fail = map(lambda mark:'P' if mark >= 30 else 'F', int_marks)
+# marks_to_pass_fail = map(lambda mark:'P' if mark >= 30 else 'F', int_marks)
 
-print(list(marks_to_pass_fail))
+# print(list(marks_to_pass_fail))
+
+# int_marks.sort()
+
+# print(int_marks)
+
+# sorted_marks = sorted(int_marks)  #Ascending
+
+# sorted_marks = sorted(int_marks, reverse=True)  #Descending
+
+# print(sorted_marks)
+
+stu_marks = {
+    "ram": 89,
+    "hari": 78,
+    "sita": 49,
+    "gita": 100
+}
+
+def return_value(item):
+    return item[1]
+
+# sorted_marks = sorted(stu_marks.items())
+# sorted_marks = sorted(stu_marks.items(), key= return_value)
+sorted_marks = sorted(stu_marks.items(), key= lambda item:item[1], reverse=True)
+
+# print(sorted_marks)
+
+ranked_stu_marks = dict(sorted_marks)
+
+print(ranked_stu_marks)
