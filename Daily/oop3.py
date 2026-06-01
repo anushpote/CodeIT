9ac54b78d182cc1160eedfffff647082a4c874c2
Anush Pote <anushpote345@gmail.com>
2026-06-01 20:57:01 +0545

    Matrix Multiplication

--- Daily/oop3.py
+++ Daily/oop3.py
@@ -18,14 +18,26 @@ class Matrix:
         return matrix_str_format
     
     def __add__(self, other):
+        if not isinstance(other, Matrix):
+            raise Exception("Second operand must be a matrix!")
         new_a = self.a + other.a
         new_b = self.b + other.b
 
         new_martix = Matrix(new_a, new_b)
         return new_martix
-
+    
+    def __sub__(self, other):
+        if not isinstance(other, Matrix):
+            raise Exception("Second operand must be a matrix!")
+        return Matrix(self.a - other.a, self.b - other.b)
+    
+    def __mul__(self, other):
+        return Matrix(self.a * other.a, self.b * other.b)
 
 a1 = Matrix(1, 2)
 a2 = Matrix(4, 3)
 
-print(a1 + a2)
+# print(a1 + a2)
+# print(a1-a2)
+
+print(a1 * a2)
